a3c973e128073a9fc83b63019f913126ab0878fc
Eduardo Tosta <eduardo_tosta@outlook.com>
2025-04-11 11:15:43 -0300

    :tada: Commit Inicial

---
+++ helloworld/src/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		/*
+		 * Forma de comentar no código
+		 * */
+		System.out.println("Hello, World!");
+		System.out.println("A Turma Java 82 é Top!");
+	}
+
+}
