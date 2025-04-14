7980e2ec2963b15be3fe4c40253113ff069d1b93
Eduardo Tosta <eduardo_tosta@outlook.com>
2025-04-14 12:18:45 -0300

    Exercicio 1 resolvido

---
+++ aula_02/src/aula_02/Ex_001.java
@@ -0,0 +1,23 @@
+package aula_02;
+
+import java.util.Scanner;
+
+public class Ex_001 {
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		System.out.printf("Digite o salário: ");
+		float salario = leia.nextFloat();
+		System.out.printf("Digite o abono: ");
+		float abono = leia.nextFloat();
+		
+		float novoSalario = salario+abono;
+		
+		System.out.printf("Novo salário: %.2f", novoSalario);
+		
+		
+		
+		
+		
+	}
+}
